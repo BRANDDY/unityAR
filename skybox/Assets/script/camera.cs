f53ba85e7379385c21f5c8636811139e26c1c73e
BRANDDY <BRANDY.M.824@GMAIL.COM>
2022-03-05 00:52:34 -0500

    reload skybox work

---
+++ skybox/Assets/script/camera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    public float rotationSpeed = 20f;
+    private Quaternion targetRotation;
+    float targetY = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRotation = Quaternion.identity; //original rotation data
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButton(0))
+        {
+            targetY += Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime; //rota level
+            targetRotation.eulerAngles = new Vector3(0, Mathf.Repeat(targetY, 360f) + targetY, 0); //get euler Angles
+            transform.rotation = targetRotation;
+        }
+    }
+}
---
+++ skybox/Assets/script/planet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class planet : MonoBehaviour
+{
+    public Transform moon;
+    public Transform planet1;
+    public Transform planet2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moon.Rotate(Vector3.up, Space.Self);
+        moon.RotateAround(Camera.main.transform.position, Vector3.up,Time.deltaTime*2);
+        rotation(planet1, moon);
+        rotation(planet2, moon);
+    }
+
+    void rotation(Transform child, Transform parent)
+    {
+        child.Rotate(Vector3.up, Space.Self);
+        child.RotateAround(parent.position, Vector3.up, Time.deltaTime * 20);
+    }
+}
